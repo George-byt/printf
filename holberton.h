d2b7034835761be714e98fafb550e1fd4c49def7
Santiago-Cano <78014751+Santiago-Cano@users.noreply.github.com>
2021-03-15 12:25:23 -0500

    Add _putchar and printescape functions. Fix spelling of <unistd.h>

--- holberton.h
+++ holberton.h
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <unisted.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
+void printescape(char a);
+int _putchar(char c);
 
 #endif /*HOLBERTON_H*/
