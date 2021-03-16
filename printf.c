35dbc9a22babd0df6ba19ae8abb17256581e9850
Santiago-Cano <78014751+Santiago-Cano@users.noreply.github.com>
2021-03-16 10:45:54 -0500

    Comply with betty, index is added by default on percent encounter.
    Added space after declarations, reduced funct_format to less than 40 lines, fixed indentation.

--- printf.c
+++ printf.c
@@ -6,15 +6,15 @@
  */
 int _printf(const char *format, ...)
 {
-<<<<<<< HEAD
-int length;
-va_list args;
-va_start(args, format);
-if (format == NULL)
-return (-1);
-length = funct_format(args, format);
-va_end(args);
-return (length);
+	int length;
+	va_list args;
+
+	va_start(args, format);
+	if (format == NULL)
+		return (-1);
+	length = funct_format(args, format);
+	va_end(args);
+	return (length);
 }
 
 /**
@@ -26,91 +26,43 @@ return (length);
  */
 int funct_format(va_list args, const char *format)
 {
-int length = 0, index;
-char c;
-for (index = 0; format[index]; index++)
-{
-if (format[index] == '%')
-{
-if (!format[index])
-return (-1);
-switch (format[index + 1])
-{
-case 'c':
-c = va_arg(args, int);
-_putchar(c);
-index++;
-break;
-case 's':
-length += print_str(args);
-length--;
-index++;
-break;
-case '%':
-_putchar('%');
-index++;
-break;
-case 'd':
-length += print_integer(args);
-index++;
-break;
-case 'i':
-length += print_integer(args);
-index++;
-break;
-default:
-_putchar('%');
-break;
-}
-}
-else
-_putchar(format[index]);
-length++;
-}
-return (length);
-=======
-  int length = 0, index;
-  va_list args;
+	int length = 0, index;
+	char c;
 
-  va_start(args, format);
-
-  for (index = 0; format != NULL && format[index] != '\0'; index++)
-    {
-      if (format[index] == '%')
-	switch (format[index + 1])
-	  {
-	  case 'c':
-	    _putchar(va_arg(args, int));
-	    index += 2;
-	    break;
-	  case 's':
-	    length += print_str(args);
-	    length--;
-	    index++;
-	    break;                                                                                                                                                       case '%':
-	    write(1, "%", 1);
-	    index += 2;
-	    break;
-	  case 'd':
-	    length += print_integer(args);
-	    index++;
-	    break;
-	  case 'i':
-	    length += print_integer(args);
-	    index++;
-	    break;
-	  default:
-	    _putchar(format[index]);
-	    index += 2;
-	    break;
-	  }
-
-      else
-	
-	  _putchar(format[index]);
-	  length++;
-       
-    }
-  return (length);
->>>>>>> main
+	for (index = 0; format[index]; index++)
+	{
+		if (format[index] == '%')
+		{
+			if (!format[index])
+				return (-1);
+			switch (format[index + 1])
+			{
+			case 'c':
+				c = va_arg(args, int);
+				_putchar(c);
+				break;
+			case 's':
+				length += (print_str(args) - 1);
+				break;
+			case '%':
+				_putchar('%');
+				break;
+			case 'd':
+				length += print_integer(args);
+				break;
+			case 'i':
+				length += print_integer(args);
+				break;
+			default:
+				_putchar('%');
+				index--;
+				break;
+			}
+			index++;
+		}
+		else
+			_putchar(format[index]);
+		length++;
+	}
+	return (length);
 }
