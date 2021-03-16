d174f0cddd8b87ee2565c05104999a22ef1921ba
Santiago-Cano <78014751+Santiago-Cano@users.noreply.github.com>
2021-03-16 10:54:31 -0500

    Modify printf_funct to be betty compliant.

--- print_funct.c
+++ print_funct.c
@@ -7,10 +7,10 @@
  */
 int print_num(int n, int c)
 {
-if (n / 10)
-c = print_num(n / 10, c);
-_putchar(n % 10 + '0');
-return (c + 1);
+	if (n / 10)
+		c = print_num(n / 10, c);
+	_putchar(n % 10 + '0');
+	return (c + 1);
 }
 /**
  * print_integer -Function that print a integer.
@@ -19,27 +19,29 @@ return (c + 1);
  */
 int print_integer(va_list arg)
 {
-int number;
-int count = 0;
-number = va_arg(arg, int);
-count = print_num(number, count);
-return (count);
+	int number;
+	int count = 0;
+
+	number = va_arg(arg, int);
+	count = print_num(number, count);
+	return (count);
 }
 /**
  * print_str - function that print a string
- *@lalista: list that contains the format inserted
+ * @arg: list that contains the format inserted
  *Return: tot
  */
 int print_str(va_list arg)
 {
-char *str;
-int i;
-str = va_arg(arg, char *);
-if (str == NULL)
-str = "(null)";
-for (i = 0; str[i] != '\0'; i++)
-{
-_putchar(str[i]);
-}
-return (i);
+	char *str;
+	int i;
+
+	str = va_arg(arg, char *);
+	if (str == NULL)
+		str = "(null)";
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	return (i);
 }
