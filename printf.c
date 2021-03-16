9b13641b9e096362775d7a75c07028b012dcf390
George-byt <jmoralesvalencia750@gmail.com>
2021-03-16 05:47:16 +0000

    Cambios printf.c

--- printf.c
+++ printf.c
@@ -2,51 +2,51 @@
 /**
  * _printf - imitate printf function
  * @format: string
- * Return: length.
+ * Return: 0
  */
 int _printf(const char *format, ...)
 {
-int length = 0, index;
-	va_list args;
+  int length = 0, index;
+  va_list args;
 
-	va_start(args, format);
+  va_start(args, format);
 
-	for (index = 0; format != NULL && format[index] != '\0'; index++)
+  for (index = 0; format != NULL && format[index] != '\0'; index++)
+    {
+      if (format[index] == '%')
+	switch (format[index + 1])
+	  {
+	  case 'c':
+	    _putchar(va_arg(args, int));
+	    index += 2;
+	    break;
+	  case 's':
+	    length += print_str(args);
+	    length--;
+	    index++;
+	    break;                                                                                                                                                       case '%':
+	    write(1, "%", 1);
+	    index += 2;
+	    break;
+	  case 'd':
+	    length += print_integer(args);
+	    index++;
+	    break;
+	  case 'i':
+	    length += print_integer(args);
+	    index++;
+	    break;
+	  default:
+	    _putchar(format[index]);
+	    index += 2;
+	    break;
+	  }
+
+      else
 	{
-		if (format[index] == '%')
-			switch (format[index + 1])
-			{
-			case 'c':
-			  _putchar(va_arg(args, int));
-			  index += 2;
-				break;
-			case 's':
-			  length += print_str(args);
-			  length--;
-			  index++;
-		       	break;
-			case '%':
-				write(1, "%", 1);
-				index += 2;
-				break;
-			case 'd':
-			  length += print_integer(args);
-			  index++;
-			  break;
-			case 'i':
-			  length += print_integer(args);
-			  index++;
-			  break;
-			default:
-				_putchar(format[index]);
-				index += 2;
-				break;
-			}
-	      
-		else
-		{
-			_putchar(format[index]);
-			length++;
-		}
+	  _putchar(format[index]);
+	  length++;
 	}
-	return (length);
+    }
+  return (length);
+}
